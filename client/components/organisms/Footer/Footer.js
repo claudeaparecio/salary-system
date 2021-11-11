b60e3c08aaef1eabe701108a6c4acdde27f78a97
Claude Aparecio <jacobaparecio@gmail.com>
2021-11-11 15:58:58 +0800

    chore

--- client/components/organisms/Footer/Footer.js
+++ client/components/organisms/Footer/Footer.js
@@ -12,7 +12,7 @@ export default function FooterComponent() {
       <Container>
         <Content className="has-text-centered">
           <p>
-            {`Copyright Ⓒ ${year} MERN Boilerplate. All Rights Reserved.`}
+            {`Copyright Ⓒ ${year} Salary System. All Rights Reserved.`}
           </p>
         </Content>
       </Container>
--- client/components/organisms/Navigation/Navigation.js
+++ client/components/organisms/Navigation/Navigation.js
@@ -48,7 +48,7 @@ export default function Navigation({ pathname }) {
             component={Link}
           >
             <Title className="logo" size="3">
-              MERN Boilerplate
+              Salary System
             </Title>
           </Navbar.Item>
           <div className="navbar-brand-right">
